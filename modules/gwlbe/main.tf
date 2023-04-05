d46970510746a9e60a1385c19c18846420f62339
sameer666 <singh.sameer9415@gmail.com>
2023-04-05 18:58:35 +0530

    gwlbe submodule update

--- modules/gwlbe/main.tf
+++ modules/gwlbe/main.tf
@@ -14,22 +14,36 @@ resource "aws_subnet" "gwlbe_subnet" {
 
 resource "aws_route_table" "gwlbe_route" {
   #count  = length(var.spoke_rt_id) != 0 ? length(var.spoke_rt_id) : 0
-  count             = length(var.gwlbe_subnet_cidr) != 0 ? (var.inbound ? 0 : length(var.gwlbe_subnet_cidr)) : 0
+  count             = length(var.gwlbe_subnet_cidr) != 0 ? length(var.gwlbe_subnet_cidr) : 0
   vpc_id = var.vpc_id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = var.ngw_id[count.index]
-  }
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   gateway_id = var.ngw_id[count.index]
+  # }
 
   tags = {
     Name = "GWLB-RT-${count.index + 1}"
   }
 }
 
+resource "aws_route" "gwlbe_ngw_route" {
+  count                  = length(var.spoke_subnet) != 0 ? (var.inbound ? 0 : length(var.spoke_subnet)) : 0
+  route_table_id         = aws_route_table.gwlbe_route[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = var.ngw_id[count.index]
+}
+
+resource "aws_route" "gwlbe_igw_route" {
+  count                  = length(var.spoke_subnet) != 0 ? (var.inbound ? length(var.spoke_subnet) : 0) : 0
+  route_table_id         = aws_route_table.gwlbe_route[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = var.internet_gateway
+}
+
 resource "aws_route_table_association" "gwlbe_association" {
   #count          = length(var.spoke_rt_id) != 0 ? length(var.spoke_rt_id) : 0
-  count          = length(var.gwlbe_subnet_cidr) != 0 ? (var.inbound ? 0 : length(var.gwlbe_subnet_cidr)) : 0
+  count          = length(var.gwlbe_subnet_cidr) != 0 ? length(var.gwlbe_subnet_cidr) : 0
   subnet_id      = length(var.gwlbe_subnet_cidr) != 0 ? aws_subnet.gwlbe_subnet[count.index].id : data.aws_subnet.gwlbe[count.index].id
   route_table_id = aws_route_table.gwlbe_route[count.index].id
 }
@@ -50,14 +64,12 @@ resource "aws_vpc_endpoint" "gwlbe" {
   }
 }
 
-
-
-# resource "aws_route" "spoke_route" {
-#   count                  = length(var.spoke_rt_id) != 0 ? length(var.spoke_rt_id) : 0
-#   route_table_id         = var.spoke_rt_id[count.index]
-#   destination_cidr_block = "0.0.0.0/0"
-#   vpc_endpoint_id     = aws_vpc_endpoint.glwbe[count.index].id
-# }
+resource "aws_route" "spoke_route" {
+  count                  = length(var.spoke_rt_id) != 0 ? length(var.spoke_rt_id) : 0
+  route_table_id         = var.spoke_rt_id[count.index]
+  destination_cidr_block = "0.0.0.0/0"
+  vpc_endpoint_id     = aws_vpc_endpoint.gwlbe[count.index].id
+}
 
 resource "aws_route_table" "igw_route" {
   depends_on = [
@@ -72,13 +84,14 @@ resource "aws_route_table" "igw_route" {
 }
 
 resource "aws_route" "spoke_igw_route" {
-  count                  = length(var.spoke_subnet) != 0 ? length(var.spoke_subnet) : 0
+  count                  = length(var.spoke_subnet) != 0 ? (var.inbound ? length(var.spoke_subnet) : 0) : 0
   route_table_id         = aws_route_table.igw_route[0].id
   destination_cidr_block = var.spoke_subnet[count.index]
-  vpc_endpoint_id        = aws_vpc_endpoint.gwlbe[0].id
+  vpc_endpoint_id        = aws_vpc_endpoint.gwlbe[count.index].id
 }
 
 resource "aws_route_table_association" "igw_rt" {
+  count          = var.inbound ? 1 : 0
   gateway_id     = var.internet_gateway
   route_table_id = aws_route_table.igw_route[0].id
 }
